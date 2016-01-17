64c37944d3cd4c730ab41586fc3769b41d9b80d1
Romarjosh Villamor <marilyndris@gmail.com>
2016-01-17 04:38:34 -0800

    Revert to its original self.

--- src/main/java/me/thundergemios10/walls/commands/AddWall.java
+++ src/main/java/me/thundergemios10/walls/commands/AddWall.java
@@ -1,37 +1,23 @@
 package me.thundergemios10.walls.commands;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import me.thundergemios10.walls.GameManager;
-import me.thundergemios10.walls.LobbyManager;
-import me.thundergemios10.walls.MessageManager;
-import me.thundergemios10.walls.SettingsManager;
 
-public class AddWall implements SubCommand{
+public class AddWall implements SubCommand {
+	public boolean onCommand(Player player, String[] args)
+	{
+		if(player.hasPermission(permission())){
+			GameManager.getInstance().setWallFromSelected(player, args[0]);
+		}
+		return true;
+	}
 
-    @Override
-    public boolean onCommand(Player player, String[] args) {
-        if(!player.hasPermission(permission()) && !player.isOp()){
-            MessageManager.getInstance().sendFMessage(MessageManager.PrefixType.ERROR, "error.nopermission", player);
-            return true;
-        }
-        else if(args.length<1){
-                MessageManager.getInstance().sendFMessage(MessageManager.PrefixType.ERROR, "error.notspecified", player, "input-Arena");
-        	return true;
-        }
-       LobbyManager.getInstance().setLobbySignsFromSelection(player, Integer.parseInt(args[0]));
-       return true;
-    }
-
-    @Override
-    public String help(Player p) {
-        return "/w setwall <id> - " + SettingsManager.getInstance().getMessageConfig().getString("messages.help.setwall", "Add a lobby stats wall for Arena <id>");
-    }
+	public String help(Player p) {
+		return "/w setwall <gameid> <wallid> - set a wall for an arena";
+	}
 
 	@Override
 	public String permission() {
-		return "walls.admin.setwall";
+		return "walls.arena.setwall";
 	}
-
-    //TODO: TAKE A W.E SELECTIONA AND SET THE LOBBY. ALSO SET LOBBY WALL
 }
